32d7cfbe0b46d12f040e11b2b938a6af46cd8f65
gastii11 <10632@holbertonstudents.com>
2025-02-14 17:57:10 +0000

    5-printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+/**
+ *main - mensaje arte
+ *Description: muestra un mensaje sobre arte
+ *Return: retorna 0
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
