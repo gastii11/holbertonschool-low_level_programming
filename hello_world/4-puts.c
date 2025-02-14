88778a54b392fb3ee1a021fb66d865d0feb899a0
gastii11 <10632@holbertonstudents.com>
2025-02-14 17:27:04 +0000

    4-puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+/**
+ *main - escribe en pantalla
+ *Description: esta funcion lo que hace es escribir en pantalla
+ *Return: retorna 0
+*/
+#include <stdio.h>
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
