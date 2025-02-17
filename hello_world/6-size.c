cddcd90e6076c8d3cd32838c02f1484a42a4aadb
gastii11 <10632@holbertonstudents.com>
2025-02-17 12:46:20 +0000

    6-size.c

---
+++ hello_world/6-size.c
@@ -0,0 +1,18 @@
+/**
+ * main - muestra mensaje en la pantalla
+ * Description: aparecen en pantalla lineas de con el comando printf
+ * Return: retorna 0
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+
+	return (0);
+
+}
