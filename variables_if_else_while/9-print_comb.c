7b8dcb5a612835803533d0f3172c1b14916a4bb7
gastii11 <10632@holbertonstudents.com>
2025-02-17 18:04:24 +0000

    9-print_comb

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+/**
+ * main - imprime numeros
+ * Description: muestra todas las combinaciones posibles
+ * Return: return 0
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		putchar(num + '0');
+
+		if (num < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		num++;
+	}
+
+	putchar('\n');
+	return (0);
+}
