b1b82369a1dcb72034fa710a45699fc784a0dbb9
gastii11 <10632@holbertonstudents.com>
2025-02-19 21:19:40 +0000

    7-print_last_digit.c

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+int print_last_digit(int n )
+{
+	int last_digit = abs(n % 10);
+
+	_putchar(last_digit + '0');
+
+	return (last_digit);
+}
