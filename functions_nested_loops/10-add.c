29a615f40f0fa33a046eb61b39a68f1209de714b
gastii11 <10632@holbertonstudents.com>
2025-02-20 17:28:11 +0000

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+/**
+ * add - hace una suma
+ * @a: numero
+ * @b: numero
+ *
+ * Description: hace la suma de dos enteros
+ * Return: retorna 0
+ */
+
+int add(int a, int b)
+{
+	int r = a + b;
+
+	return (r);
+}
