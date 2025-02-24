18ee6203e294a605e86f9c12149adb4f8314e3ab
gastii11 <10632@holbertonstudents.com>
2025-02-24 14:27:51 +0000

    0-isupper.c

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,16 @@
-/*
- * _isupper - es mayuscula
- *
- * Description: si c es mayor a A retorna 1 y si es menor retorn 0
- * Return: return 1 si es es, return 0 en lo contario
- */
 #include "main.h"
-#include <stdio.h>
+
+/**
+ * _isupper - muetras mayusculas
+ * @c: es el caracter
+ *
+ * Return: 1 if uppercase 0 otherwise
+ */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}
-	else
-	{
-		return (0);
-	}
+	return (0);
 }
