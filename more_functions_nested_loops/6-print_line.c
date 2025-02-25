377959cb50898d518b7d93f260b686357dd28161
gastii11 <10632@holbertonstudents.com>
2025-02-25 12:40:50 +0000

    6-print_line.c

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+/**
+ * print_line - lines recta
+ *@n: caracter
+ *
+ * Return: retorna linea
+ */
+#include "main.h"
+
+void print_line(int n)
+{
+	int i;
+		if (n < 0)
+		{
+			for (i = 0; i < n; i++)
+			{
+				_putchar('_');
+			}
+			_putchar('\n');
+		}
+}
