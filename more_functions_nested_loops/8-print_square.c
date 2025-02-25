0b7243b677e364a33c5e5de86bc1b57420969afd
gastii11 <10632@holbertonstudents.com>
2025-02-25 15:53:43 +0000

    8-print_square.c

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+/**
+ * print_square - cuadrado
+ * @size: lados del cuadrado
+ *
+ * Return: retorna un cuadrado
+ */
+#include "main.h"
+void print_square(int size)
+{
+	int i, j;
+	
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
