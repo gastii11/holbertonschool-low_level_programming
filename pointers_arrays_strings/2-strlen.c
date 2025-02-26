04c908af9f2609cfdbf794e44ed474817b389c7b
gastii11 <10632@holbertonstudents.com>
2025-02-26 16:20:27 +0000

    2-strlen.c

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+/**
+ * _strlen - cadena
+ * @s: caracter
+ *
+ * Return: retorna i
+ */
+#include "main.h"
+int _strlen(char *s)
+{
+	int i = 0;
+	int c = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		c += 1;
+	}
+	return (c);
+	
+}
