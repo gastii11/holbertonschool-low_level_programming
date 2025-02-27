c912b41dd780d721aa9bf368c8266d5c1926df76
gastii11 <10632@holbertonstudents.com>
2025-02-27 16:23:08 +0000

    6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+/**
+ * puts2 - cadena cada dos caracteres
+ * @str: variable
+ *
+ * Return: retorna 0
+ */
+#include "main.h"
+#include <stddef.h>
+void puts2(char *str)
+{
+	int i;
+
+	if (str == NULL)
+		return;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
