197c6781531974004ff7796cf90b2875bd873a3e
gastii11 <10632@holbertonstudents.com>
2025-02-28 18:09:55 +0000

    1-strncat.c

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+/**
+ * _strncat - concatena
+ * @dest: puntero
+ * @src: puntero
+ * @n: variable
+ *
+ * Return: retorna dest
+ */
+#include "main.h"
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len = 0;
+	int i = 0;
+
+	while (dest[dest_len] != '\0')
+	{
+		dest_len++;
+	}
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+
+	dest[dest_len + i] = '\0';
+
+	return dest;
+}
