b22fd14b3667c6765856f7e3b6c720862d8a3eaa
gastii11 <10632@holbertonstudents.com>
2025-02-28 18:42:52 +0000

    2-strncpy.c

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+/**
+ * _strncpy - copiado
+ * @dest: puntero
+ * @src: puntero
+ * @n: variable
+ *
+ * Return: dest
+ */
+#include "main.h"
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
