3d67b91dfee1226833c789acb48b167c864f19be
gastii11 <10632@holbertonstudents.com>
2025-02-28 20:47:45 +0000

    4-rev_array.c

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+/**
+ * reverse_array - invierte un contenido
+ * @a: puntero
+ * @n: variable
+ *
+ * Return: retorna 0 
+ */
+#include "main.h"
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int temp;
+
+	for (i = 0; i < n / 2; i++;)
+	{
+		temp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = temp;
+	}
+}
