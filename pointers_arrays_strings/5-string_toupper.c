dabf441eb3ce2c690b68a027d2eaa8cce5986942
gastii11 <10632@holbertonstudents.com>
2025-03-01 18:41:28 +0000

    5-string_toupper.c

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,1 +1,20 @@
+/**
+ * string_toupper - mayusculas de a - z
+ * @str: puntero
+ *
+ * Return: retorna str
+ */
+#include "main.h"
+char *string_toupper(char *str)
+{
+	int i = 0;
 
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
