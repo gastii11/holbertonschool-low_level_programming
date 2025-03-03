ed66834afa4cec0145aa15d7b5b2bb884aedd4cf
gastii11 <10632@holbertonstudents.com>
2025-03-03 19:10:12 +0000

    7-print_chessboard.c

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+/**
+ * print_chessboard - funcion
+ * @a: puntero
+ *
+ */
+#include "main.h"
+#include <stdio.h>
+void print_chessboard(char (*a)[8])
+{
+	int i = 0;
+	int j = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			printf("%c", a[i][j]);
+		}	
+		printf("\n");
+		
+	}
+}
