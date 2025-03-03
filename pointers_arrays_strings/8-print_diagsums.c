f7b731be9435c39631f58d483977531949157d17
gastii11 <10632@holbertonstudents.com>
2025-03-03 19:34:35 +0000

    8-print_diagsums.c

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+/**
+ * print_diagsums - funcion
+ * @a: puntero
+ * @size: caracter
+ *
+ */
+#include "main.h"
+#include <stdio.h>
+void print_diagsums(int *a, int size)
+{
+	int i = 0;
+	int suma1 = 0;
+	int suma2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		suma1 += a[i * size + i];
+		suma2 += a[i * size + (size - 1 - i)];
+	}
+
+	printf("Suma de la diagonal principal: %d\n", suma1);
+	printf("Suma de la diagonal secundaria: %d\n", suma2);
+
+}
