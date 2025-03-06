c8150b66015d6c6e7c4f90b0db06c5e4dc9d0422
gastii11 <10632@holbertonstudents.com>
2025-03-06 14:05:24 +0000

    0-whatsmyname.c

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+/**
+ * main - funcion
+ * @argv: vector de argumentos
+ * 
+ * Return: retorna 0
+ */
+#include "main.h"
+#include <stdio.h>
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
