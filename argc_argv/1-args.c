dfdd3c975278bcfc03ae327a4890806254cbe0fd
gastii11 <10632@holbertonstudents.com>
2025-03-06 16:00:23 +0000

    1-agars

---
+++ argc_argv/1-args.c
@@ -0,0 +1,18 @@
+/**
+ * main - funcion
+ * @argv: vector de argumentos
+ * @argc: recuentro de argumento
+ * Return: retorna 0
+ */
+#include "main.h"
+#include <stdio.h>
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	int num_arguments;
+
+	num_arguments = argc - 1;
+	printf("%d\n", num_arguments);
+
+	return (0);
+}
