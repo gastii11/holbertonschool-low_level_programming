e5dd934bd93c5bebe11a44f02551774d7ba12810
gastii11 <10632@holbertonstudents.com>
2025-03-11 18:22:58 +0000

    2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -26,11 +26,10 @@ char *str_concat(char *s1, char *s2)
 
 	while (s1[len1] != '\0')
 		len1++;
-	
+
 	while (s2[len2] != '\0')
 		len2++;
 
-	
 	result = malloc((len1 + len2 + 1) * sizeof(char));
 	if (result == NULL)
 	{
