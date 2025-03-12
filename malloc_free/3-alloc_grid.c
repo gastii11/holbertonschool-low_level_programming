da40004d58970ec6c48022bdb4068885fde4bedf
gastii11 <10632@holbertonstudents.com>
2025-03-12 17:27:00 +0000

    3-alloc_grid.c

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,17 +1,43 @@
+/**
+ * alloc_grid - funcion
+ * @width: anchura
+ * @height: altura
+ *
+ * Return: retorna grid
+ */
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 int **alloc_grid(int width, int height)
 {
 	int **grid;
-	int i;
+	int i, j;
 
-	if (width <= 0) || (height <= 0)
+	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
 
-	**grid = malloc(width * sizeof(int *));
+	grid = malloc(width * sizeof(int *));
 	if (grid == NULL)
 	{
 		return (NULL);
 	}
 
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(grid[j]);
+			free(grid);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+	return (grid);
+}
