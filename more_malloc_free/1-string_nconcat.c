fcf2c376309d9ee0215a367e44e523fdca537207
gastii11 <10632@holbertonstudents.com>
2025-03-14 14:00:17 +0000

    1-string_nconcat.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -40,7 +40,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (j = 0; j < n; j++)
 		result[len1 + j] = s2[j];
 	result[len1 + n] = '\0';
-
+	
+	free(result);
 	return (result);
 }
 
