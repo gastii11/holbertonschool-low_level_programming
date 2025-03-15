fc000d54a4ed4424b84702346096612d8a9ef671
gastii11 <10632@holbertonstudents.com>
2025-03-15 20:40:18 +0000

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - estructura dog
+ * @name: nombre del perro
+ * @age: edad del perro
+ * @owner: due√o del perro
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
