755af9f05e3c89027ffb51d8bc728e8e45abe23d
gastii11 <10632@holbertonstudents.com>
2025-03-15 21:33:07 +0000

    1-init_dog.c

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stddef.h>
+/**
+ * void init_dog - funcion
+ * @d: puntero
+ * @name: puntero
+ * @age: entero
+ * @owner: puntero
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
