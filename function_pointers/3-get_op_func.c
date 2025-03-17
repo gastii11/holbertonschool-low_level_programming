db70e740be24173a98e3a3da60a5caa081d502db
gastii11 <10632@holbertonstudents.com>
2025-03-17 15:29:37 +0000

    3-op_functions.c

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,3 +1,10 @@
+#include <stddef.h>
+/**
+ * get_op_func - funcion
+ * @s: puntero
+ *
+ * Return: retorna NULL
+ */
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
@@ -11,4 +18,12 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
+	while (ops[i].op != NULL)
+	{
+		if (ops[i].op[0] == s[0] && s[1] == '\0')
+			return (ops[i].f);
+		i++;
+	}
 
+	return (NULL);
+}
---
+++ function_pointers/3-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	int (*operation)(int, int);
+
+
