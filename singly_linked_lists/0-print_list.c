9c5f773b91732b786e3325f69c2c7958c84b8908
gastii11 <10632@holbertonstudents.com>
2025-04-01 18:50:15 +0000

    0-print_list.c

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * imprimir_lista - Imprime todos los elementos de una lista list_t.
+ * @h: Puntero al inicio de la lista.
+ *
+ * Return: Número total de nodos en la lista.
+ */
+size_t print_list(const list_t *h)
+{
+	size_t contador = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+
+		h = h->next;
+		contador++;
+	}
+
+	return (contador);
+}
