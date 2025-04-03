a0f1cd260634b7f5264b19314b2b026ddb7522be
gastii11 <10632@holbertonstudents.com>
2025-04-03 16:20:37 +0000

    3-add_node_end.c

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -47,7 +47,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	temp = *head;
 	while (temp->next != NULL)
 	{
-		emp = temp->next;
+		temp = temp->next;
 	}
 	temp->next = new_node;
 	return (new_node);
