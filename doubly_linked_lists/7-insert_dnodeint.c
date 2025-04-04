9de69ee07abf4c4c5c847613500916c2897547d6
gastii11 <10632@holbertonstudents.com>
2025-04-04 18:27:14 +0000

    7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -47,8 +47,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (temp->next != NULL)
 		temp->next->prev = new_node;
-
 	temp->next = new_node;
-
 	return (new_node);
 }
