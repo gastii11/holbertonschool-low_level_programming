4dd299890ab4b02a55559b10625e0a287b7b1033
gastii11 <10632@holbertonstudents.com>
2025-04-04 14:43:44 +0000

    3-add_dnodeint_end.c

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -17,18 +17,18 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->n= n;
+	new_node->n = n;
 	new_node->next = NULL;
 	new_node->prev = NULL;
 
 	if (*head == NULL)
-	{	
+	{
 		*head = new_node;
 		return (new_node);
 	}
 
 	temp = *head;
-	
+
 	while (temp->next != NULL)
 	{
 		temp = temp->next;
