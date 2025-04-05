32fbddca49e7e6777e1fd21992ef0219b6828b9c
gastii11 <10632@holbertonstudents.com>
2025-04-05 19:20:01 +0000

    0-binary_to_uint.c

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -2,7 +2,7 @@
 /**
  * binary_to_uint - Convierte un número binario en un unsigned int.
  * @b: Puntero a una cadena de caracteres '0' y '1'.
- * 
+ *
  * Return: El número convertido, o 0 si:
  */
 unsigned int binary_to_uint(const char *b)
@@ -12,7 +12,7 @@ unsigned int binary_to_uint(const char *b)
 
 	if (!b)
 		return (0);
-	
+
 	num = 0;
 	for (i = 0; b[i]; i++)
 	{
@@ -20,6 +20,6 @@ unsigned int binary_to_uint(const char *b)
 			return (0);
 		num = (num * 2) + (b[i] - '0');
 	}
-	
+
 	return (num);
 }
